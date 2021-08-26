527c980a1e64d21774a605d91046ecde0a2ec00d
GemVV <gemxmas@gmail.com>
2021-08-26 13:31:04 +0300

    l05

--- l05/string.php
+++ l05/string.php
@@ -2,22 +2,51 @@
 
 $name = 'Dmytro';
 
-echo 'test string<br>';
-echo "test $name string<br>";
-echo 'test',    $name,  'string<br>';
+echo 'Test $name string<br>';
+echo 'Test ', $name, ' string<br>';
+echo 'Test ' . $name . ' string<br>';
+echo "Test $name string<br>";
+echo('Test<br><br>');
 
+print 'Test $name string<br>';
+print "Test $name string<br>";
+print('Test<br><br>');
 
-
-print 'test $name string<br>';
-print "test $name string<br>";
-
-$val = print 'test $name string<br>';
+$val = print 'Test $name string<br>';
 var_dump($val);
-//HERODOC
+print '<br>';
+print '<br>';
+
+// HEREDOC
 $sql = <<<SQL
-select *
-from user  
-where name = '$name'
+    SELECT *
+    FROM users
+    WHERE name = '$name'
 SQL;
 echo $sql, '<br>';
-//NARODOC
+
+$html = <<<HTML
+<div class="test"><span>QQQQQ</span></div>
+HTML;
+var_dump($html);
+
+$t = <<<QWERTY
+test message
+with many lines
+QWERTY;
+echo $t;
+echo '<br>';
+
+// NOWDOC
+$sql2 = <<<'SQL'
+    SELECT *
+    FROM users
+    WHERE name = '$name'
+SQL;
+echo $sql2, '<br><br>';
+
+$js = <<<JS
+document.addEventListener('click', () => { alert(1) });
+JS;
+echo $js;
+//HARADOC
