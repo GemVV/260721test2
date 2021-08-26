ac9e951333b193cc2d1d737f59d7fd448309a9eb
GemVV <gemxmas@gmail.com>
2021-08-26 13:26:01 +0300

    game

--- l06/guess-game.php
+++ l06/guess-game.php
@@ -1,14 +1,16 @@
 <?php
 
-if (isset($_POST['number']))
-$number = (int)$_POST['number'];
+if (isset($_POST['number'])) {
+    $number = (int)$_POST['number'];
+} else {
+    $number = 0;
+}
+
 $randomNumber = random_int(1, 10);
 
-$randomNumber = $number === $rabdomNumber;
+$isSuccess = $number === $randomNumber;
 
 ?>
-
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -16,17 +18,24 @@ $randomNumber = $number === $rabdomNumber;
     <title>Guess Game</title>
 </head>
 <body>
-    <h1>Guess Game</h1>
-    <?php if ($isSuccess) : ?>
-    <P>Yuo are WINNER</P>
-    <P>You are LOOSER. Our number is <?= $randomNumber</P>
-    <?php endif ?>
+<h1>Guess Game</h1>
 
+<?php if ($isSuccess) : ?>
+    <p>You are WINNER!</p>
+<?php else : ?>
+    <p>You are LOOSER. Our number is <?= $randomNumber ?></p>
+<?php endif ?>
 
-    <form method="post">
-        <label for="number-input">Enter number from 1 to 10</label>
-        <input type="number" name="number" min="1" max="10" id="number-input">
-            name
-    </form>
+<form method="post">
+    <label for="number-input">Enter number from 1 to 10</label>
+    <input type="number"
+           name="number"
+           min="1"
+           max="10"
+           value="<?= $number ?>"
+           id="number-input">
+
+    <button type="submit">GUESS</button>
+</form>
 </body>
 </html>
