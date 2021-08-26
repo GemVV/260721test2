57b9f7116ff37b109d478db6141fb82d2e59b38b
GemVV <gemxmas@gmail.com>
2021-08-26 13:32:45 +0300

    l04

--- l04/tags_and_comments.php
+++ l04/tags_and_comments.php
@@ -1,5 +1,28 @@
+test message
 <?php
-echo 123;
+
+// Чувак, бери деньги вперед, заказчик мудак
+# Чувак, бери деньги вперед, заказчик мудак
+
+/*
+ * you can write
+ * multi-line comments
+ */
+
+echo 123 + 44; // counting bonuses
+echo 123 + 44; # counting bonuses
+
+/**
+ * @var string $test
+ */
+
+//var_dump($test);
+
 ?>
 <h1>PHP EXAMPLE</h1>
-<p>User <?=Dmitro</p>
+<p>User <?php echo 'Dmytro' ?> has $<?php echo 100 ?></p>
+<p>User <?= 'Dmytro' ?> has $<?= 100 ?></p>
+
+<?php
+
+echo 44444, '<br>', 5555;
